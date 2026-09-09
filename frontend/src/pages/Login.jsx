7cf2a4bf8e3bb24f485e69207b9d342679dbe56c
Shota0714 <u.shota0714@gmail.com>
2026-09-09 15:52:47 -0400

    added backend and frontend configuration for Vercel deployment; updated API URLs to use environment variables

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
         const dataObj = Object.fromEntries(data);
 
         try {
-            const res = await axios.post('http://localhost:3000/api/v1/auth/login', dataObj);
+            const res = await axios.post(`${import.meta.env.VITE_BACKEND}/api/v1/auth/login`, dataObj);
             setError(false);
             localStorage.setItem('token', res.data.token);
             navigate('/dashboard');
